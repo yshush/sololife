109adeb553baee4648dac2a6e27f63359438fba5
yshush <tjdgns7533@gmail.com>
2022-04-07 11:51:26 +0900

    [step.7-3] 게시판 수정/삭제 - 로그아웃

--- app/src/main/java/com/yshush/mysololife/MainActivity.kt
+++ app/src/main/java/com/yshush/mysololife/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.yshush.mysololife
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.yshush.mysololife.auth.IntroActivity
+import com.yshush.mysololife.setting.SettingActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +22,11 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<ImageView>(R.id.settingBtn).setOnClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
 
         /*
         findViewById<Button>(R.id.logoutBtn).setOnClickListener {
